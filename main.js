09925ad7309ff9a80d8c1e45f633b5c4c8810a16
kathyhernndez <katherinehcontact@gmail.com>
2024-09-21 11:04:36 -0400

    all section setup

--- main.js
+++ main.js
@@ -29,3 +29,36 @@ ScrollReveal().reveal(".header__container h1", {
     ...scrollRevealOption,
     delay: 500,
 });
+
+// about container
+
+ScrollReveal().reveal(".about__image img", {
+    ...scrollRevealOption,
+    origin: "left",
+});
+
+ScrollReveal().reveal(".about__content .section__subheader", {
+    ...scrollRevealOption,
+    delay: 500,
+});
+
+ScrollReveal().reveal(".about__content .section__header", {
+    ...scrollRevealOption,
+    delay: 1000,
+});
+
+ScrollReveal().reveal(".about__content .section__description", {
+    ...scrollRevealOption,
+    delay: 1500,
+});
+
+ScrollReveal().reveal(".about__btn", {
+    ...scrollRevealOption,
+    delay: 2000,
+});
+
+// room container
+ScrollReveal().reveal(".room__card", {
+    ...scrollRevealOption,
+    interval: 500,
+  });
